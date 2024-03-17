d7beab5e3cd117249ee855f47317c5db53d066d5
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-17 23:18:25 +0300

    adding SectionsLayout protocol

---
+++ Utilities/Extensions/Collection+.swift
@@ -0,0 +1,22 @@
+//
+//  Collection+.swift
+//  News
+//
+//  Created by Ibrahim Nasser Ibrahim on 17/03/2024.
+//
+
+import Foundation
+
+public extension Collection {
+    /// Safely retrieves an element at the specified index, if it exists.
+    ///
+    /// - Parameter index: The index of the element to retrieve.
+    /// - Returns: The element at the specified index, or `nil` if the index is out of bounds.
+    subscript(safe index: Index) -> Element? {
+        /// Checks if the collection contains the specified index.
+        ///
+        /// If the index is within the valid range of indices for the collection,
+        /// returns the element at that index. Otherwise, returns `nil`.
+        indices.contains(index) ? self[index] : nil
+    }
+}

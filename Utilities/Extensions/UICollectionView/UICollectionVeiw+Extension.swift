0cf5caa60e08a20668ac5b057e6bd8661eeb68b5
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-17 23:18:05 +0300

    adding UICollectionView Extension

---
+++ Utilities/Extensions/UICollectionView/UICollectionVeiw+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UICollectionVeiw+Extension.swift
+//  News
+//
+//  Created by Ibrahim Nasser Ibrahim on 17/03/2024.
+//
+
+import UIKit
+protocol Identifiable { }
+
+extension Identifiable {
+    static var identifier: String { String(describing: Self.self) }
+}
+
+extension UICollectionView {
+    func registerNib<Cell: UICollectionViewCell>(_ cell: Cell.Type) where Cell: Identifiable {
+        self.register(UINib(nibName: Cell.identifier, bundle: nil),
+                      forCellWithReuseIdentifier: Cell.identifier)
+    }
+    
+    func dequeue<Cell: UICollectionViewCell>(indexPath: IndexPath) -> Cell? where Cell: Identifiable {
+        return self.dequeueReusableCell(withReuseIdentifier: Cell.identifier, for: indexPath) as? Cell
+    }
+}

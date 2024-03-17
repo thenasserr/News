ff97964d75f1e9573f00f09cd171c65a40e38f99
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-17 23:17:08 +0300

    adding app router

---
+++ Utilities/Router/AppRouter.swift
@@ -0,0 +1,75 @@
+//
+//  AppRouter.swift
+//  News
+//
+//  Created by Ibrahim Nasser Ibrahim on 17/03/2024.
+//
+
+import UIKit
+
+public final class AppRouter {
+    public let navigationController: UINavigationController
+    
+    public required init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        navigationController.navigationBar.tintColor = .black
+    }
+}
+
+extension AppRouter: Router {
+    public func present(_ viewController: UIViewController, animated: Bool = true, completion: @escaping () -> Void = {}) {
+        navigationController.present(viewController, animated: animated, completion: completion)
+    }
+    
+    public func presentFullScreen(
+        _ viewController: UIViewController,
+        animated: Bool = true,
+        completion: @escaping () -> Void = {}
+    ) {
+        viewController.modalPresentationStyle = .fullScreen
+        self.present(viewController, animated: animated, completion: completion)
+    }
+    
+    public func presentOverFullScreen(
+        _ viewController: UIViewController,
+        animated: Bool = true,
+        completion: @escaping () -> Void = {}
+    ) {
+        viewController.modalPresentationStyle = .overFullScreen
+        self.present(viewController, animated: animated, completion: completion)
+    }
+    
+    public func dismiss(animated: Bool = true, completion: @escaping () -> Void = {}) {
+        if navigationController.presentedViewController != nil {
+            navigationController.dismiss(animated: animated, completion: completion)
+        } else {
+            navigationController.popViewController(animated: animated)
+            completion()
+        }
+    }
+    
+    public func push(_ viewController: UIViewController, animated: Bool = true, completion: @escaping () -> Void = {}) {
+        navigationController.dismiss(animated: false)
+        navigationController.pushViewController(viewController, animated: animated)
+        completion()
+    }
+    
+    public func reset(completion: @escaping () -> Void) {
+        navigationController.dismiss(animated: false)
+        navigationController.viewControllers.removeAll()
+    }
+    
+    public func popToViewController(
+        _ viewController: UIViewController,
+        animated: Bool = true,
+        completion: @escaping () -> Void = {}
+    ) {
+        navigationController.popToViewController(viewController, animated: animated)
+        completion()
+    }
+    
+    public func popToRoot(animated: Bool = true, completion: @escaping () -> Void = {}) {
+        navigationController.popToRootViewController(animated: animated)
+        completion()
+    }
+}

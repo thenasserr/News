d0acc1067d67f812c9b6bffc90d52bc65af8b301
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-17 22:47:50 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Ibrahim Nasser Ibrahim on 17/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

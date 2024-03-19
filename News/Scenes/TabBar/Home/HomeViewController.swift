bfad5e37c9f8aaff35fbdadaf47bafe6296ccc56
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-19 03:58:06 +0300

    try fetch request

--- News/Scenes/TabBar/Home/HomeViewController.swift
+++ News/Scenes/TabBar/Home/HomeViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import Combine
 
 class HomeViewController: UIViewController {
+    private var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+        let request = APIRequest(endpoint: .topHeadlines, query: nil)
+        APIService.shared.fetchNews(request: request, type: NewsModelResponse.self)
+            .sink { _ in
+                // Handle completion
+            } receiveValue: { response in
+                print(response)
+            }
+            .store(in: &cancellables)
     }
 }

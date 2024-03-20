d8a4e19874b9af954505d66c1166fed43d23167c
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-20 05:24:23 +0300

    finishing Breaking News Section

---
+++ News/Scenes/TabBar/Home/Presentation/Sections/BreakingNewsSection/BreakingNewsCollectionViewCell.swift
@@ -0,0 +1,47 @@
+//
+//  BreakingNewsCollectionViewCell.swift
+//  News
+//
+//  Created by Ibrahim Nasser Ibrahim on 19/03/2024.
+//
+
+import UIKit
+
+class BreakingNewsCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var publishedDate: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var newsTitle: UILabel!
+    @IBOutlet weak var titleImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureUI()
+    }
+
+    func setup(new: Article) {
+        let imageURL = new.urlToImage ?? ""
+        titleImage.load(urlString: imageURL)
+        name.text = new.source.name
+        publishedDate.text = new.publishedAt
+        descriptionLabel.text = new.description
+    }
+    
+    private func configureUI() {
+        titleImage.layer.cornerRadius = 15
+        clipsToBounds = false
+        configureLabelsUI()
+    }
+    
+    private func configureLabelsUI() {
+        name.textColor = .darkGray
+        name.font = .h3
+        
+        publishedDate.textColor = .darkGray
+        publishedDate.font = .h3
+        
+        descriptionLabel.textColor = .black
+        descriptionLabel.font = .regular
+        
+    }
+}
---
+++ News/Scenes/TabBar/Home/Presentation/Sections/BreakingNewsSection/BreakingNewsSection.swift
@@ -0,0 +1,116 @@
+//
+//  BreakingNewsSection.swift
+//  News
+//
+//  Created by Ibrahim Nasser Ibrahim on 20/03/2024.
+//
+
+import UIKit
+
+protocol BreakingNewsSectionDelegate: AnyObject {
+    func breakingNewsSection(_ section: BreakingNewsSection, didSelect item: Article)
+}
+
+class BreakingNewsSection: SectionsLayout {
+    
+    typealias ItemsType = Article
+    
+    var items: [ItemsType] = []
+    
+    var sectionHeaderTitle: String
+    
+    weak var delegate: BreakingNewsSectionDelegate?
+    
+    init(items: [ItemsType], delegate: BreakingNewsSectionDelegate, sectionHeaderTitle: String) {
+        self.items = items
+        self.delegate = delegate
+        self.sectionHeaderTitle = sectionHeaderTitle
+    }
+    
+    func numberOfItems() -> Int {
+        items.count
+    }
+    
+    func sectionLayout(
+        _ collectionView: UICollectionView,
+        layoutEnvironment: NSCollectionLayoutEnvironment
+    ) -> NSCollectionLayoutSection {
+        // Item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        // Group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.7), heightDimension: .absolute(300))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let header = createHeader()
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 15
+        section.contentInsets = .init(top: 0, leading: 20, bottom: 20, trailing: 20)
+        section.boundarySupplementaryItems = [header]
+        section.orthogonalScrollingBehavior = .groupPaging
+        return section
+    }
+    
+    private func createHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                heightDimension: .estimated(50))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(
+            layoutSize: headerSize,
+            elementKind: HeaderCollectionReusableView.elementKind,
+            alignment: .top
+        )
+        
+        header.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        return header
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell: BreakingNewsCollectionViewCell = collectionView.dequeue(indexPath: indexPath) else {
+            return UICollectionViewCell()
+        }
+        cell.setup(new: items[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let item = items[indexPath.item]
+        delegate?.breakingNewsSection(self, didSelect: item)
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        viewForSupplementaryElementOfKind kind: String,
+        at indexPath: IndexPath
+    ) -> UICollectionReusableView {
+        guard let header = collectionView.dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: HeaderCollectionReusableView.identifier,
+            for: indexPath
+        ) as? HeaderCollectionReusableView else {
+            return UICollectionReusableView()
+        }
+        header.setupHeaderTitle(title: sectionHeaderTitle)
+        header.setupHeaderButtonTitle(buttonTitle: "See All")
+        return header
+    }
+    
+    func registerCell(in collectionView: UICollectionView) {
+        collectionView.registerNib(BreakingNewsCollectionViewCell.self)
+    }
+    
+    func registerSupplementaryView(in collectionView: UICollectionView) {
+        collectionView.register(HeaderCollectionReusableView.self,
+                                forSupplementaryViewOfKind: HeaderCollectionReusableView.elementKind,
+                                withReuseIdentifier: HeaderCollectionReusableView.identifier)
+    }
+    
+    func registerDecorationView(layout: UICollectionViewLayout) {
+        
+    }
+}

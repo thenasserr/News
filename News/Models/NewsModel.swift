d626d7d5b652d0d2463719cd0a966704610de743
Ibrahim Nasser <thenasserr@gmail.com>
2024-03-20 22:08:44 +0300

    adding explore model

--- News/Models/NewsModel.swift
+++ News/Models/NewsModel.swift
@@ -22,3 +22,22 @@ struct Article: Codable {
 struct Source: Codable {
     let name: String
 }
+
+struct Explore: Codable {
+    let title: String
+    let image: String
+}
+
+extension Explore {
+    static let mockData: [Explore] = [
+        .init(title: "Politics", image: "explore/1"),
+        .init(title: "Tech", image: "explore/2"),
+        .init(title: "Sport", image: "explore/3"),
+        .init(title: "Finance", image: "explore/4"),
+        .init(title: "Business", image: "explore/5"),
+        .init(title: "Fashion", image: "explore/6"),
+        .init(title: "Health", image: "explore/7"),
+        .init(title: "Crypto", image: "explore/8"),
+        .init(title: "Entert", image: "explore/9")
+    ]
+}
